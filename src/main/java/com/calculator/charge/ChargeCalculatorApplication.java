00ee8ef97f91f04b26d7e5505b2dc02245fde13a
Andrew <grot0136@umn.edu>
2019-10-08 17:26:51 -0400

    Adding main application

---
+++ src/main/java/com/calculator/charge/ChargeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.charge;
+
+import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChargeCalculatorApplication.class, args);
+
+    }
+}
