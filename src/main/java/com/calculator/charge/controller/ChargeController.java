b81b7fb409fd07a7f17cc1688a2a400ee2580f7b
Andrew <grot0136@umn.edu>
2019-10-09 23:02:36 -0400

    Setting chargeAmount before passing it off to success page

--- src/main/java/com/calculator/charge/controller/ChargeController.java
+++ src/main/java/com/calculator/charge/controller/ChargeController.java
@@ -1,16 +1,27 @@
 package com.calculator.charge.controller;
 
 import com.calculator.charge.model.Appointment;
+import com.calculator.charge.service.ChargeService;
+import com.calculator.charge.validator.AppointmentValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 
 @Controller
 public class ChargeController {
+
+    @Autowired
+    AppointmentValidator validator;
+
+    @Autowired
+    ChargeService chargeService;
 
     @GetMapping("/chargeform")
     public String loadSitterForm(Model model) {
@@ -20,10 +31,14 @@ public class ChargeController {
     }
 
     @PostMapping("/chargeform")
-    public String processSitterForm(@Valid Appointment appointment, BindingResult result) {
+    //public String processSitterForm(@Valid Appointment appointment, BindingResult result) {
+    public String processSitterForm(@Valid @ModelAttribute("appointment") Appointment appointment, BindingResult result) {
+        validator.validate(appointment, result);
+
         if(result.hasErrors()) {
             return "chargeform";
         }
+        appointment.setChargeAmount(chargeService.calculateCharge(appointment));
         return "chargeconfirm";
     }
 }
