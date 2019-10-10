ded055f91a0ac869dad4b9a2ec04265de23f4406
Andrew <grot0136@umn.edu>
2019-10-10 15:52:45 -0400

    adding unit tests for main application

---
+++ src/test/java/com/calculator/charge/ChargeApplicationTest.java
@@ -0,0 +1,16 @@
+package com.calculator.charge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChargeApplicationTest {
+
+    @Test
+    public void main() {
+        ChargeCalculatorApplication.main(new String[] {});
+    }
+}
