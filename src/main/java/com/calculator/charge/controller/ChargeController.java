f13f234307a860fb06f6256cf13a6595a1e712e2
Andrew <grot0136@umn.edu>
2019-10-10 16:17:06 -0400

    add full constructor for test use

--- src/main/java/com/calculator/charge/controller/ChargeController.java
+++ src/main/java/com/calculator/charge/controller/ChargeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,11 @@ public class ChargeController {
 
     @Autowired
     ChargeService chargeService;
+
+    public ChargeController(AppointmentValidator validator, ChargeService chargeService) {
+        this.validator = validator;
+        this.chargeService = chargeService;
+    }
 
     @GetMapping("/chargeform")
     public String loadSitterForm(Model model) {
