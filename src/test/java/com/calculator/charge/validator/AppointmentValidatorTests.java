eb5eaa99bd696816be33e9460c5dd64ea6dce04b
Andrew <grot0136@umn.edu>
2019-10-10 16:05:27 -0400

    cleaning up unused imports

--- src/test/java/com/calculator/charge/validator/AppointmentValidatorTests.java
+++ src/test/java/com/calculator/charge/validator/AppointmentValidatorTests.java
@@ -8,11 +8,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
 
 import javax.xml.ws.RequestWrapper;
 import java.time.LocalTime;
